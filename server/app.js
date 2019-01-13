d76d8f758a9c1b5663ebae288d72b812fcac38fe
Tyler James Greve <tylerjamesgreve@gmail.com>
2019-01-13 12:21:04 -0800

    created a simple node server to serve images. Added more variables to css, added meta tags, snippets

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 /* Express server to host json data */
 
-//import * from './test.json';
 const express = require('express');
 const app = express();
 const port = 3000;
 const data = require('./../assets/data/dogs.json');
+
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://mongodb://tjg:practice1@ds255784.mlab.com:55784/adoption';
 
 app.get('/data', (req, res) => res.send(data));
 
