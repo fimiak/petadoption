e1373687ead20784049bdaa38151e4f65a1dee00
Tyler James Greve <tylerjamesgreve@gmail.com>
2019-01-13 16:36:02 -0800

    Added nodemon, added grid item construction in fetch request, now overlay will only work after fetch returns

--- server/app.js
+++ server/app.js
@@ -4,10 +4,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const data = require('./../assets/data/dogs.json');
-
+/*
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb://mongodb://tjg:practice1@ds255784.mlab.com:55784/adoption';
+*/
 
-app.get('/data', (req, res) => res.send(data));
+/* Set headers for CORS requests on local server */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+/* Get the data.json when accessed by this route */
+app.get('/data', (req, res, next) => res.send(data));
 
 app.listen(port, () => console.log(`Listening on port ${port}.`));
